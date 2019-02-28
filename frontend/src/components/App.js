ca588ff87fb501044d5acbffc3a80749a5af391b
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2019-02-28 19:24:51 +0100

    Dodanie linków

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { withRouter, Route } from 'react-router-dom';
+import { withRouter, Route, Link } from 'react-router-dom';
 import Login from './Login';
+import Test from './Test';
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Link to="/">Login</Link> <br />
+        <Link to="/test">Test</Link>
         <Route path="/" exact strict component={Login}/>
+        <Route path="/test" exact strict component={Test}/>
       </div>
     );
   }
